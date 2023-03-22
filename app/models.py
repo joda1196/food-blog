46bbd363afed3fcb63e6cb9f04c0f5b76126c665
Luis Ortiz <luis.ortiz0320@gmail.com>
2023-03-21 20:09:56 -0500

    added Profile model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    bio = models.TextField(null=True, blank=True)
+
