5c3794a51e1445b7d8ea48aa6d0de7a674169e80
Luis Ortiz <luis.ortiz0320@gmail.com>
2023-03-21 20:31:30 -0500

    MORE TESTING

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50, null=True)
     bio = models.TextField(null=True, blank=True)
