4ab5f2870989b7488ad008e37e98a8bab8e3c324
Armando Quinonez <josearmando9825@hotmail.com>
2023-03-21 20:26:52 -0500

    Added Comment Model

--- app/models.py
+++ app/models.py
@@ -8,9 +8,15 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=50, null=True)
     bio = models.TextField(null=True, blank=True)
 
+
 class BlogPost(models.Model):
     Title = models.CharField(max_length=50)
     Author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now=False, auto_now_had=True)
     updated_at = models.DateField(auto_now=False, auto_now_had=True)
 
+
+class Comment(models.Model):
+    Author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateField(auto_now=False, auto_now_had=True)
