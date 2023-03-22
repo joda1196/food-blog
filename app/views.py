95feb2503a64a9c2f26989492e41530a76248f2c
joda1196 <jhtrott629@gmail.com>
2023-03-22 11:18:31 -0500

    homepage

--- app/views.py
+++ app/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def test(request):
-    context = {'test':'testing_django'}
-    return render(request, 'test.html',context)
+    context = {"test": "testing_django"}
+    return render(request, "test.html", context)
+
+
+def homepage(request):
+    return render(request, "homepage.html")
