257578edb28b39c2deeb6757dfdf8c1812b589db
Armando Quinonez <josearmando9825@hotmail.com>
2023-03-21 20:11:22 -0500

    testing

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    Title = models.CharField(max_length=50)
+    Author = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now=False, auto_now_had=True)
+    updated_at = models.DateField(auto_now=False, auto_now_had=True)
